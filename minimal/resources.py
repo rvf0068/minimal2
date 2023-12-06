941e0de1a9ab47d7949b5c9a3804c6f520b2e497
Rafael Villarroel <rvf0068@gmail.com>
2023-12-06 14:42:02 -0600

    Add files via upload

---
+++ minimal/resources.py
@@ -0,0 +1,7 @@
+"""test resources"""
+
+
+import pkg_resources
+
+imagen = pkg_resources.resource_filename('minimal', '/data/bomba.png')
+texto = pkg_resources.resource_filename('minimal', '/data/texto.txt')
