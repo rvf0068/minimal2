49726b084612519d1ed28ac5b69625bc9164b08d
Rafael Villarroel <rvf0068@gmail.com>
2023-12-06 16:07:44 -0600

    Update resources.py

--- minimal/resources.py
+++ minimal/resources.py
@@ -3,5 +3,5 @@
 
 import pkg_resources
 
-imagen = pkg_resources.resource_filename('minimal', '/data/bomba.png')
+imagen = pkg_resources.resource_filename('minimal', '/data/bomba.jpg')
 texto = pkg_resources.resource_filename('minimal', '/data/texto.txt')
